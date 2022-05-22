f5b6475fed210a10c14fd3b822079a3f19d6ae96
Oğuzhan KARACABAY <oguzhan.karacabay@tarfin.com>
2022-05-22 18:35:35 +0300

    Added `BasicModel` class

---
+++ tests/Models/BasicModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Oguzhankrcb\AutoCastingJsonResource\Tests\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BasicModel extends Model
+{
+    protected $guarded = [];
+}
