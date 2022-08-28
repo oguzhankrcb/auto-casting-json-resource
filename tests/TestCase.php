b681505c89d51a938f4ee2e8f9735eb60efff0a4
Oğuzhan KARACABAY <oguzhan.karacabay@tarfin.com>
2022-08-28 18:36:25 +0300

    Remove unnecessary things

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,35 +1,22 @@
 <?php
 
-namespace Oguzhankrcb\AutoCastingJsonResource\Tests;
+namespace Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Foundation\Testing\WithFaker;
+use Orchestra\Testbench\TestCase as BaseTestCase;
 use Oguzhankrcb\AutoCastingJsonResource\AutoCastingJsonResourceServiceProvider;
 
-class TestCase extends \PHPUnit\Framework\TestCase
+class TestCase extends BaseTestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Oguzhankrcb\\AutoCastingJsonResource\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
             AutoCastingJsonResourceServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_auto-casting-json-resource_table.php.stub';
-        $migration->up();
-        */
     }
 }
